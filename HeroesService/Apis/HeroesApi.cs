12a2389bf3f7c2dd3541fd6cf1cd0d13a62b85e2
4atg89 <18016490+4atg89@users.noreply.github.com>
2025-06-01 21:37:04 +0400

    start implement heroes

---
+++ HeroesService/Apis/HeroesApi.cs
@@ -0,0 +1,26 @@
+using HeroesService.Heroes;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeroesService.Apis;
+
+public static class HeroesApi
+{
+
+    public static IEndpointRouteBuilder MapHeroesApi(this IEndpointRouteBuilder app)
+    {
+        var vApi = app.NewVersionedApi("Heroes");
+        var api = vApi.MapGroup("api/heroes");
+
+        api.MapGet("/", (Delegate)GetHeroes);
+        return app;
+    }
+
+
+    public static async Task<Results<Ok<Barbarian>, NotFound, BadRequest<ProblemDetails>>> GetHeroes(HttpContext context)
+    {
+        Console.WriteLine("HeroHeroHero");
+        await Task.Delay(500);
+        return TypedResults.Ok(new Barbarian());
+    }
+}
