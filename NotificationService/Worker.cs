5973400e71f33e9a8ce505f43e8b1226e36e70ea
4atg89 <18016490+4atg89@users.noreply.github.com>
2025-06-13 16:20:36 +0400

    add restart for the rabbitmq

--- NotificationService/Worker.cs
+++ NotificationService/Worker.cs
@@ -6,6 +6,7 @@ public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
     private readonly IRabbitMqConsumer _rabbitMqConsumer;
+
     public Worker(ILogger<Worker> logger, IRabbitMqConsumer rabbitMqConsumer)
     {
         _logger = logger;
@@ -14,6 +15,28 @@ public class Worker : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        await _rabbitMqConsumer.Subscribe("notifications-exchange", "email-queue", "send.email", (m) => Console.WriteLine($"consumed -> {m}"));
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            try
+            {
+                _logger.LogInformation("Starting RabbitMQ");
+
+                await _rabbitMqConsumer.Subscribe("notifications-exchange", "email-queue", "send.email", (message) =>
+                    {
+                        _logger.LogInformation("Received notification: {Message}", message);
+                        //todo redo later on
+                        Console.WriteLine($"consumed -> {message}");
+                    }
+                );
+
+                _logger.LogWarning("RabbitMQ subscription ended");
+                await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error in RabbitMQ subscription");
+                await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+            }
+        }
     }
 }
